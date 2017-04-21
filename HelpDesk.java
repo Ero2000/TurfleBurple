5f4b251cb982033652e05a6dcb8011196e730bdb
Edward Ro <ero@stuy.edu>
2017-04-21 07:06:49 -0400

    Added HelpDesk.java skeleton

---
+++ HelpDesk.java
@@ -0,0 +1,12 @@
+public class HelpDesk {
+    //Variables
+    private ArrayPriorityQueue data;
+    private ArrayList services;
+
+    //Constructor
+    public HelpDesk(){
+	data = new ArrayPriorityQueue();
+	services = new ArrayList();
+    }
+
+}
