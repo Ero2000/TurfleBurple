20e4833d6bcae8c2d091067d39e08911f59b024b
adrisj7 <adrisj7@gmail.com>
2017-04-27 08:39:39 -0400

    Add input and basic functionality for HelpDesk

--- HelpDesk.java
+++ HelpDesk.java
@@ -1,21 +1,53 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class HelpDesk {
     //Variables
     private ArrayPriorityQueue data;
     private ArrayList services;
 
+    private Scanner sc;
+
+    private static int last_id;
+
     //Constructor
-    public HelpDesk(){
-	data = new ArrayPriorityQueue();
-	services = new ArrayList();
-	services.add("passwordReset");
-	services.add("dispatchTech");
-	services.add("rebootOpSystem");
+    public HelpDesk() {
+	    data = new ArrayPriorityQueue();
+	    services = new ArrayList();
+	    sc = new Scanner(System.in);
+        services.add("passwordReset");
+	    services.add("dispatchTech");
+	    services.add("rebootOpSystem");
     }
 
     public Ticket createTicket(String username, double priority, int ID, String problemDescription) {
         Ticket retTicket = new Ticket(username, priority, ID, problemDescription);
-	return retTicket;
+	    return retTicket;
+    }
+
+    private String inputString(String prompt) {
+        System.out.print(prompt + " ");
+        return sc.next(); 
+    }
+    
+    private int inputInt(String prompt) {
+        System.out.print(prompt + " ");
+        return sc.nextInt(); 
+    }
+
+    private boolean inputYN(String prompt) {
+        System.out.print(prompt + " y/n: ");
+        return (sc.next().equals("y"));
+    }
+
+    private void prompt() {
+        String name = inputString("What is your name?");
+        String problem = inputString("Describe your problem");
+        
+    }
+
+    public static void main(String[] args) {
+        
+
     }
 }
