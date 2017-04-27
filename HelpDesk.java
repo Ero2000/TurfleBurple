d5500fead8e8c807504dbf17649e41fdd210611c
adrisj7 <adrisj7@gmail.com>
2017-04-27 09:31:12 -0400

    Add example stuff and helpful methods for HelpDesk. Framework is more or less there

--- HelpDesk.java
+++ HelpDesk.java
@@ -8,7 +8,8 @@ public class HelpDesk {
 
     private Scanner sc;
 
-    private static int last_id;
+    private static int lastID = 0;
+
 
     //Constructor
     public HelpDesk() {
@@ -20,12 +21,13 @@ public class HelpDesk {
 	    services.add("rebootOpSystem");
     }
 
-    public Ticket createTicket(String username, double priority, int ID, String problemDescription) {
-        Ticket retTicket = new Ticket(username, priority, ID, problemDescription);
-	    return retTicket;
+    public Ticket createTicket(String username, double priority, String problemDescription) {
+        Ticket retTicket = new Ticket(username, priority, lastID, problemDescription);
+	    lastID++;
+        return retTicket;
     }
 
-    private String inputString(String prompt) {
+       private String inputString(String prompt) {
         System.out.print(prompt + " ");
         return sc.next(); 
     }
@@ -35,14 +37,52 @@ public class HelpDesk {
         return sc.nextInt(); 
     }
 
+    // Returns true/false for a yes/no prompt
     private boolean inputYN(String prompt) {
         System.out.print(prompt + " y/n: ");
         return (sc.next().equals("y"));
     }
 
-    private void prompt() {
+    private int inputOptions(String prompt, String options...) {
+        int result = -1;
+        while (result != -1) {
+            System.out.println(prompt "(Pick an option from 1 to " + options.length + ": ");
+            for(int i = 0; i < options.length; i++) {
+                System.out.println(options[i] + " (" + (i+1) + ")");
+            }
+            result = sc.nextInt();
+            if (result == -1) System.out.println("Please pick a valid number");
+        }
+        return result;
+    }
+
+    // TODO: Fill in
+    private String callService(String service) {
+        return "";
+    }
+
+    // Gos through questions. Returns the final problem
+    private String prompt() {
         String name = inputString("What is your name?");
         String problem = inputString("Describe your problem");
+        if (!inputYN("Is your device plugged in?")) {
+            return "Plug it in";
+        }
+        if (!inputYN("Did you turn it on?")) {
+            return "Turn it on. Push the on button dummy";
+        }
+        if ( inputOptions("What is your OS?", "Linux", "Windows", "Mac OSX", "Other") != 1) {
+            return "Get a better OS";
+        }
+        if (!inputYN("Are you able to log in?") {
+            if (!inputYN("Do you have a user account setup?")) {
+                return callService("Acccount Setup"); // TODO: Make this mean something
+            }
+            if (!inputYN("Do you know your password?")) {
+                return callService("Password Reset"); // TODO: Make this mean something
+            }
+            return "
+        }
         
     }
 
